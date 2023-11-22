b587fa38c90bb2af4b74050d598e238273723cb0
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-22 10:12:27 -0300

    feat: Aula 03 - Trabalhando com Arrays.

---
+++ build/arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Trabalhando com Arrays
+ */
+let dados = ["Joao", "Bia", "Caio"];
+let dados2 = ["Joao", "Bia", "Caio"];
+// Trabalhando com Arrays de Multi Types
+let infos = ["Pedro", 23];
---
+++ build/variaveis.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * Variaveis
+ */
+// tipos primitivos: boolean, number, string
+let ligado = false;
+let nome = "Alex";
+let idade = 20;
+let altura = 1.70;
+// tipos especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+//tipos abrangentes: any, void
+let retornoView = false;
+let retorno;
+//objeto - sem previsibilidade
+let produto = {
+    name: "Alex",
+    cidade: "Sao Paulo",
+    idade: 30
+};
+let meuProduto = {
+    nome: "Sapato",
+    preco: 89.99,
+    unidades: 5
+};
