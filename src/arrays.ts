8c26b1b91f10b27293d75233b0360ab08545ccd6
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-22 10:11:12 -0300

    feat: Aula 03 - Trabalhando com Arrays

---
+++ src/arrays.ts
@@ -0,0 +1,9 @@
+/**
+ * Trabalhando com Arrays
+ */
+
+let dados: string[] = ["Joao", "Bia", "Caio"];
+let dados2: Array<string> = ["Joao", "Bia", "Caio"];
+
+// Trabalhando com Arrays de Multi Types
+let infos: (string | number)[] = ["Pedro", 23];
